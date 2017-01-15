2e3a55dcfc119bf5ce1b19b2bb362301ec41caef
Dan Sajjad <dan.sajjad@gmail.com>
2017-01-15 00:29:27 -0800

    Add speakers route

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,28 @@ const appdata = require('../data.json');
 /* GET home page. */
 router.get('/', (req, res, next) => {
     
-    let myArtwork = [];    
-    appdata.speakers.forEach(({artwork}) => {
-      myArtwork = myArtwork.concat(artwork);
-    });
+  let myArtwork = [];    
+  appdata.speakers.forEach(({artwork}) => {
+    myArtwork = myArtwork.concat(artwork);
+  });
 
   res.render('index', {
     title: 'Home',
     artwork: myArtwork 
+  });
+});
+
+/*Speakers page*/
+router.get('/speakers', (req, res, next) => {
+
+  let myArtwork = [];    
+  appdata.speakers.forEach(({artwork}) => {
+    myArtwork = myArtwork.concat(artwork);
+  });
+
+  res.render('speakers', {
+    title: 'Speakers', 
+    artwork: myArtwork
   });
 });
 
