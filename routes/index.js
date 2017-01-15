4102791b9192460185053a9eda10f61a8c116133
Dan Sajjad <dan.sajjad@gmail.com>
2017-01-14 23:58:47 -0800

    Add artwork data to home route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
+const appdata = require('../data.json')
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    
+    let myArtwork = [];
+    
+    appdata.speakers.forEach(({artwork}) => {
+      myArtwork = myArtwork.concat(artwork);
+    });
+    
+    title: 'Home',
+    artwork: myArtwork 
+  });
 });
 
 module.exports = router;
