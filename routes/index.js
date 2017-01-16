8a8a273e7b0f0823860b4062aa302ad6400e04aa
Dan Sajjad <dan.sajjad@gmail.com>
2017-01-15 23:10:21 -0800

    Pull out code related to getting all artwork from each route into one function

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const appdata = require('../data.json');
 
+//pull out artwork from data.json
+const getAllArtwork = () => {
+  let myArtwork = [];
+  appdata.speakers.forEach(({ artwork }) => {
+    myArtwork = myArtwork.concat(artwork)
+  });
+  return myArtwork;
+}  
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    
-  let myArtwork = [];    
-  appdata.speakers.forEach(({artwork}) => {
-    myArtwork = myArtwork.concat(artwork);
-  });
+
+  let myArtwork = getAllArtwork();
 
   res.render('index', {
     title: 'Home',
@@ -19,11 +25,8 @@ router.get('/', (req, res, next) => {
 /*Speakers page*/
 router.get('/speakers', (req, res, next) => {
 
-  let myArtwork = [];    
-  appdata.speakers.forEach(({artwork}) => {
-    myArtwork = myArtwork.concat(artwork);
-  });
-
+  let myArtwork = getAllArtwork();
+  
   res.render('speakers', {
     title: 'Speakers', 
     artwork: myArtwork
